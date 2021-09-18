88439a376d1faa3cee2c445814dd1de76417c72d
Brujo Benavides <elbrujohalcon@gmail.com>
2021-09-18 12:10:48 +0200

    ➕🔘🗣

--- src/beamoji_prv.erl
+++ src/beamoji_prv.erl
@@ -12,19 +12,36 @@ init(State) ->
                           {bare, true},
                           {deps, []},
                           {example, "rebar3 🪄"},
-                          {opts, []},
+                          {opts, opts()},
                           {short_desc, "blow your mind with emojis"},
                           {desc, "🪄📜 ⇢ 📃✨ ⇢ 🤯"}]),
     {ok, rebar_state:add_provider(State, Provider)}.
 
+opts() ->
+    [{translator,
+      $🗣,
+      "translator",
+      {atom, beamoji_id_translator},
+      "Translator module to use when emojifying the code"}].
+
 %% @private
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()}.
 do(State) ->
-    rebar_api:info("emojifying your code...", []),
+    Translator = get_translator(State),
+    rebar_api:info("emojifying your code with ~p...", [Translator]),
     FormatOptions = rebar_state:get(State, format, []),
+    FormatterOptions = proplists:get_value(options, FormatOptions, #{}),
     NewState =
-        rebar_state:set(State, format, [{formatter, beamoji_formatter} | FormatOptions]),
+        rebar_state:set(State,
+                        format,
+                        [{formatter, beamoji_formatter},
+                         {options, FormatterOptions#{translator => Translator}}
+                         | FormatOptions]),
     rebar3_format_prv:do(NewState).
+
+get_translator(State) ->
+    {Args, _} = rebar_state:command_parsed_args(State),
+    proplists:get_value(translator, Args, beamoji_id_translator).
 
 %% @private
 -spec format_error(any()) -> binary().
