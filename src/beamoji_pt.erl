200bdaee7be16293093ab15ae09292289b50ce89
Mariano Guerra <mariano@marianoguerra.org>
2021-09-18 12:16:28 +0200

    🔡⏩🔠

---
+++ src/beamoji_pt.erl
@@ -0,0 +1,27 @@
+-module(beamoji_pt).
+
+-export([parse_transform/2, parse_transform_info/0, walker/2]).
+
+-type '👀'() :: term().
+
+% Brujo would be proud
+-callback walker('👀'(), '👀'()) -> {'👀'(), '👀'()}.
+
+parse_transform(Forms0, Options) ->
+    #{translator_mod := TranslatorMod} = Options,
+    TranslatorState = TranslatorMod:'🐣'(),
+    TranslatorFn = fun TranslatorMod:'⏩'/2,
+    State = #{translator_fn => TranslatorFn, translator_state => TranslatorState},
+    {Forms, _NewState} = ast_walk:forms(Forms0, fun walker/2, State),
+    Forms.
+
+parse_transform_info() ->
+    #{error_location => column}.
+
+walker(State, {atom, Line, Atom}) ->
+    #{translator_fn := TranslatorFn, translator_state := TranslatorState} = State,
+    NewAtom = TranslatorFn(Atom, TranslatorState),
+    NewAst = {atom, Line, NewAtom},
+    {NewAst, State};
+walker(State, Ast) ->
+    {Ast, State}.
---
+++ test/beamoji_pt_SUITE.erl
@@ -0,0 +1,28 @@
+-module(beamoji_pt_SUITE).
+
+-compile(export_all).
+
+all() ->
+    [identity_pt, walker_works].
+
+data_dir(Config) ->
+    proplists:get_value(data_dir, Config).
+
+identity_pt(Config) ->
+    DataDir = data_dir(Config),
+    ModPath = filename:join(DataDir, "pt_test_1.erl"),
+    {ok, Forms} = epp:parse_file(ModPath, []),
+    Opts = #{translator_mod => beamoji_id_translator},
+    TransformedForms = beamoji_pt:parse_transform(Forms, Opts),
+    Forms = TransformedForms.
+
+walker_works(_Config) ->
+    TranslatorState = i_am_the_translator_state,
+    ReverseAtomFn =
+        fun(UnquotedAtom, i_am_the_translator_state) ->
+           Str = atom_to_list(UnquotedAtom),
+           list_to_atom(lists:reverse(Str))
+        end,
+    State1 = #{translator_fn => ReverseAtomFn, translator_state => TranslatorState},
+    {{atom, 42, cba}, State1} = beamoji_pt:walker(State1, {atom, 42, abc}),
+    {{integer, 10, 42}, State1} = beamoji_pt:walker(State1, {integer, 10, 42}).
