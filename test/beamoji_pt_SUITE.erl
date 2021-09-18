2b4d35a5f882abbaabefddd77247686c54dcb017
Mariano Guerra <mariano@marianoguerra.org>
2021-09-18 15:39:31 +0200

    🧑‍🏭

--- test/beamoji_pt_SUITE.erl
+++ test/beamoji_pt_SUITE.erl
@@ -3,7 +3,10 @@
 -export([all/0, identity_pt/1, walker_works/1]).
 
 all() ->
-    [identity_pt, walker_works].
+    [identity_pt,
+     walker_works,
+     walker_attr_updates_translator,
+     beamoji_attr_updates_translator].
 
 data_dir(Config) ->
     proplists:get_value(data_dir, Config).
@@ -12,7 +15,7 @@ identity_pt(Config) ->
     DataDir = data_dir(Config),
     ModPath = filename:join(DataDir, "pt_test_1.erl"),
     {ok, Forms} = epp:parse_file(ModPath, []),
-    Opts = #{translator_mod => beamoji_id_translator},
+    Opts = [],
     TransformedForms = beamoji_pt:parse_transform(Forms, Opts),
     Forms = TransformedForms.
 
@@ -26,3 +29,24 @@ walker_works(_Config) ->
     State1 = #{translator_fn => ReverseAtomFn, translator_state => TranslatorState},
     {{atom, 42, cba}, State1} = beamoji_pt:walker(State1, {atom, 42, abc}),
     {{integer, 10, 42}, State1} = beamoji_pt:walker(State1, {integer, 10, 42}).
+
+walker_attr_updates_translator(Config) ->
+    Attr = {attribute, 1, beamoji_translator, beamoji_emojilist_translator},
+    {Attr, #{translator_fn := Fn, translator_state := _}} = beamoji_pt:walker(#{}, Attr),
+    true = is_function(Fn).
+
+parse_test_module(Config, FileName) ->
+    DataDir = data_dir(Config),
+    ModPath = filename:join(DataDir, FileName),
+    {ok, Forms} = epp:parse_file(ModPath, []),
+    Forms.
+
+beamoji_attr_updates_translator(Config) ->
+    Forms = parse_test_module(Config, "pt_test_2.erl"),
+    ExpectedForms = parse_test_module(Config, "pt_test_2_translated.erl"),
+    Opts = [],
+    TransformedForms = beamoji_pt:parse_transform(Forms, Opts),
+    compare_mod_asts(TransformedForms, ExpectedForms).
+
+compare_mod_asts([_FileAttr1, _ModAttr1 | Ast1], [_FileAttr2, _ModAttr2 | Ast1]) ->
+    true.
---
+++ test/beamoji_pt_SUITE_data/pt_test_2_translated.erl
@@ -0,0 +1,16 @@
+-module(beamoji_pt_2_translated).
+
+-beamoji_translator(beamoji_emojilist_translator).
+
+-export([thinking_face/1, thinking_face/0]).
+
+thinking_face(true) ->
+    false;
+thinking_face(false) ->
+    true;
+thinking_face(undefined) ->
+    thinking_face(false).
+
+thinking_face() ->
+    L = [thinking_face, {thinking_face}, #{thinking_face => thinking_face}],
+    thinking_face:thinking_face(L).
